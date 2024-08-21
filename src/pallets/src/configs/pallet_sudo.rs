9987962e2cc2b65fe3b9609e4e810550e37c429d
Polkaverse <pankajchaudhary172@gmail.com>
2024-08-21 11:59:55 +0530

    added sudo pallet config

---
+++ src/pallets/src/configs/pallet_sudo.rs
@@ -0,0 +1,71 @@
+use super::super::types::*;
+use chrono::prelude::*;
+use chrono::Local;
+
+#[derive(Debug, Clone, Copy)]
+pub enum PalletSudoTraits {
+    RuntimeEvent,
+    RuntimeCall,
+    WeightInfo
+}
+
+#[derive(Debug, Clone)]
+pub struct PalletSudoConfig {
+    pub name: String,
+    pub metadata: PalletMetadata,
+    pub runtime: PalletRuntimeConfig,
+    pub dependencies: PalletDependencyConfig,
+}
+
+impl PalletSudoConfig {
+    pub fn new() -> Self {
+        let pallet_description = "A pallet to provide a way to execute privileged runtime calls using a specified sudo (“superuser do”) account.";
+
+        PalletSudoConfig {
+            name: String::from("pallet_sudo"),
+            metadata: PalletMetadata {
+                size: 5078,
+                updated: Local::now().to_string(),
+                license: Some(String::from("Apache-2.0")),
+                compatibility: SubstrateVersion::Two,
+                authors: vec![CommonAuthors::ParityTechnologies],
+                categories: Some(vec![PalletCategories::Consensus].into()),
+                short_description: String::from("FRAME Sudo consensus pallet"),
+                description: String::from(pallet_description),
+            },
+            runtime: PalletRuntimeConfig {
+                pallet_traits: vec![
+                    (String::from("RuntimeEvent"), String::from("RuntimeEvent")),
+                    (String::from("RuntimeCall"), String::from("RuntimeCall")),
+                    (String::from("WeightInfo"), String::from("pallet_sudo::weights::SubstrateWeight<Runtime>")),
+                ].into_iter().collect(),
+
+                additional_runtime_lib_code: None,
+                construct_runtime: PalletConstructRuntimeConfig {
+                    generic: None,
+                },
+                genesis_config: Some(PalletGenesisConfig {
+                    config_struct_name: String::from("sudo"),
+                    struct_fields: vec![
+                        ("key".to_string(), "Some(root_key)".to_string()),
+                    ].into_iter().collect(),
+                }),
+                additional_chain_spec_code: None,
+            },
+            dependencies: PalletDependencyConfig {
+                pallet: CargoComplexDependency {
+                    package: String::from("pallet-sudo"),
+                    version: None,
+                    alias: String::from("pallet-sudo"),
+                    default_features: None,
+                    git_repo: Some("https://github.com/paritytech/polkadot-sdk.git".to_string()),
+                    tag: Some("polkadot-v1.14.0".to_string()),
+                    branch: None,
+                },
+                additional_pallets: None,
+                additional_deps: None,
+            },
+        }
+    }
+}
+
