be00b980918397e70b034a1ef2efd080bdf3406a
Polkaverse <pankajchaudhary172@gmail.com>
2024-08-23 23:05:26 +0530

    added config for grandpa pallet

--- src/pallets/src/lib.rs
+++ src/pallets/src/lib.rs
@@ -1,10 +1,11 @@
 pub mod types;
 pub mod utils;
 pub mod configs {
-    pub mod pallet_utility;
     pub mod pallet_aura;
+    pub mod pallet_grandpa;
     pub mod pallet_sudo;
     pub mod pallet_timestamp;
+    pub mod pallet_utility;
 
     pub mod pallet_balances;
 }
