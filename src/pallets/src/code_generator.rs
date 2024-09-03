62f36f8b8a1ec43b358ed6a57582d0a9a16ac9ed
Polkaverse <pankajchaudhary172@gmail.com>
2024-09-03 19:25:01 +0530

    code cleanup

--- src/pallets/src/code_generator.rs
+++ src/pallets/src/code_generator.rs
@@ -1,3 +1,4 @@
+use actix_web::{web, App, HttpServer};
 use super::configs::pallet_aura::PalletAuraConfig;
 use super::configs::pallet_balances::PalletBalancesConfig;
 use super::configs::pallet_timestamp::PalletTimestampConfig;
@@ -10,7 +11,6 @@ use super::utils::file_manager::{
 use super::utils::manifest::ManifestPalletConfig;
 use super::utils::manifest::SubstrateManifestUtil;
 use super::utils::runtime::{GeneratedRuntime, SubstrateRuntimeUtil};
-use actix_web::{web, App, HttpServer};
 use std::path::Path;
 use std::process;
 
@@ -18,36 +18,11 @@ pub fn generate_project() {
     let pallet_utility_config = PalletUtilityConfig::new();
     // println!("Pallet Utility Config: {:?}", pallet_utility_config);
 
-    // Specify the file path
-    let runtime_file_path = "../../templates/solochain/basic/runtime/src/lib.rs";
-
-    let chainspec_file_path = "../../templates/solochain/basic/node/src/chain_spec.rs";
-
-    // Call the function to read the file
-    let runtime_string = read_file_to_string(runtime_file_path).unwrap();
-
-    let chainspec_string = read_file_to_string(chainspec_file_path).unwrap();
-
-    // println!("File content:\n{}", runtime_string);
-    // println!("File content:\n{}", chainspec_string);
-
-    let config = PalletConfig {
-        name: pallet_utility_config.name,
-        metadata: pallet_utility_config.metadata,
-        runtime: pallet_utility_config.runtime,
-        dependencies: pallet_utility_config.dependencies.clone(),
-    };
-
-    let mut util = SubstrateRuntimeUtil::new(config, runtime_string, chainspec_string);
-    // println!("{:#?}", util);
-    let updated_runtime = util.generate_code();
-    // println!("{}", updated_runtime.updated_runtime_code);
-
     let base_path = Path::new("../../generated_code");
     let folder_name = "pankaj";
 
     match create_new_folder(base_path, folder_name) {
-        Ok(_) => println!("Operation completed successfully."),
+        Ok(_) => println!("Created new project folder!"),
         Err(e) => eprintln!("Error creating folder: {}", e),
     }
 
@@ -56,10 +31,19 @@ pub fn generate_project() {
     let dest = Path::new(&path);
 
     match copy_dir_recursive(src, dest) {
-        Ok(_) => println!("Folder cloned successfully!"),
-        Err(e) => eprintln!("Error cloning folder: {}", e),
+        Ok(_) => println!("Project created successfully!"),
+        Err(e) => eprintln!("Error in creating the project: {}", e),
     }
 
+    // Specify the file path
+    let runtime_file_path = "../../templates/solochain/basic/runtime/src/lib.rs";
+    let chainspec_file_path = "../../templates/solochain/basic/node/src/chain_spec.rs";
+
+    // Call the function to read the file from template for code generation.
+    let runtime_string = read_file_to_string(runtime_file_path).unwrap();
+    let chainspec_string = read_file_to_string(chainspec_file_path).unwrap();
+
+    /// Generating the manifest file for the required pallets.
     let pallet_config = ManifestPalletConfig {
         name: "utility_pallet".to_string(),
         dependencies: pallet_utility_config.dependencies.clone(),
@@ -86,10 +70,30 @@ pub fn generate_project() {
         }
     }
 
-    let file_path = Path::new("../../generated_code/pankaj/runtime/src/lib.rs");
+    /// Generating the Runtime and chainspec file for the required pallets.
+    let config = PalletConfig {
+        name: pallet_utility_config.name,
+        metadata: pallet_utility_config.metadata,
+        runtime: pallet_utility_config.runtime,
+        dependencies: pallet_utility_config.dependencies.clone(),
+    };
 
-    match replace_file_content(file_path, &updated_runtime.updated_runtime_code) {
-        Ok(_) => println!("Operation completed successfully."),
-        Err(e) => eprintln!("Error replacing file content: {}", e),
+    let mut util = SubstrateRuntimeUtil::new(config, runtime_string, chainspec_string);
+    // println!("{:#?}", util);
+    let updated_code = util.generate_code();
+    // println!("{}", updated_runtime.updated_runtime_code);
+
+    /// Replacing the Runtime from new runtime generated.
+    let file_path = Path::new("../../generated_code/pankaj/runtime/src/lib.rs");
+    match replace_file_content(file_path, &updated_code.updated_runtime_code) {
+        Ok(_) => println!("Runtime updated successfully."),
+        Err(e) => eprintln!("Error replacing Runtime content: {}", e),
+    }
+
+    /// Replacing the chain spec file from new chain spec generated.
+    let file_path = Path::new("../../generated_code/pankaj/node/src/chain_spec.rs");
+    match replace_file_content(file_path, &updated_code.updated_chain_spec_code) {
+        Ok(_) => println!("Chain spec updated successfully."),
+        Err(e) => eprintln!("Error replacing Chain spec content: {}", e),
     }
 }
