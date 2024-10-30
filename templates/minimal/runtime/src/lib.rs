f950d8ce26f8d8c56031b114f6903edbcc8a9699
Polkaverse <pankajchaudhary172@gmail.com>
2024-10-30 14:33:08 +0530

    pallet vesting integration

--- templates/minimal/runtime/src/lib.rs
+++ templates/minimal/runtime/src/lib.rs
@@ -23,6 +23,9 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
+use frame::traits::WithdrawReasons;
+use frame::traits::ConvertInto;
+
 use frame::{
 	deps::frame_support::{
 		genesis_builder_helper::{build_state, get_preset},
@@ -129,6 +132,9 @@ mod runtime {
 	/// A minimal pallet template.
 	#[runtime::pallet_index(5)]
 	pub type Template = pallet_minimal_template::Pallet<Runtime>;
+
+	#[runtime::pallet_index(6)]
+	pub type Vesting = pallet_vesting;
 }
 
 parameter_types! {
@@ -170,6 +176,26 @@ impl pallet_transaction_payment::Config for Runtime {
 
 // Implements the types required for the template pallet.
 impl pallet_minimal_template::Config for Runtime {}
+
+
+parameter_types! {
+    pub const MinVestedTransfer: Balance = 100 * DOLLARS;
+    pub UnvestedFundsAllowedWithdrawReasons: WithdrawReasons =
+        WithdrawReasons::except(WithdrawReasons::TRANSFER | WithdrawReasons::RESERVE);
+}
+
+impl pallet_vesting::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type Currency = Balances;
+	type BlockNumberToBalance = ConvertInto;
+	type MinVestedTransfer = MinVestedTransfer;
+	type WeightInfo = pallet_vesting::weights::SubstrateWeight<Runtime>;
+	type UnvestedFundsAllowedWithdrawReasons = UnvestedFundsAllowedWithdrawReasons;
+	type BlockNumberProvider = System;
+	// `VestingInfo` encode length is 36bytes. 28 schedules gets encoded as 1009 bytes, which is the
+	// highest number of schedules that encodes less than 2^10.
+	const MAX_VESTING_SCHEDULES: u32 = 28;
+}
 
 type Block = frame::runtime::types_common::BlockOf<Runtime, SignedExtra>;
 type Header = HeaderFor<Runtime>;
